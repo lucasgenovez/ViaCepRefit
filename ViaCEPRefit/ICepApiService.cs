2d822beb5047f807ed14ccb294625fb3026d08ac
Lucas Genovez <lucasgenovez@outlook.com>
2022-07-27 20:29:17 -0300

    WIP

---
+++ ViaCEPRefit/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ViaCEPRefit
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ ViaCEPRefit/Program.cs
@@ -0,0 +1,31 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ViaCEPRefit
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu CEP: ");
+                string cepInformado = Console.ReadLine().ToString().Replace("-", "");
+                Console.WriteLine($"CEP Informado: {cepInformado}");
+
+                Console.WriteLine("Consultando Informações do CEP...");
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"\nLogradouro: {address.Logradouro}\nBairro: {address.Bairro}\nCidade: {address.Localidade}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro na consulta de CEP {ex.Message}");
+            }
+        }
+    }
+}
